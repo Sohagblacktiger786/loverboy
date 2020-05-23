338651775cb03011019aa6239517913d6370fbc5
N S SoHag Chowdhury <65599236+Sohagblacktiger786@users.noreply.github.com>
2020-05-23 12:22:26 +0600

    Update user.py

--- user.py
+++ user.py
@@ -88,7 +88,7 @@ print  """\033[1;91m=======================================
 print " \x1b[1;93m============================================================="
 
 CorrectUsername = "sohag"
-CorrectPassword = "sohag"
+CorrectPassword = "worldhackingteam"
 
 loop = 'true'
 while (loop == 'true'):
